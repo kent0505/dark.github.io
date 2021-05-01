c4dcd41144d5c5251bf6e2208c615e9843119400
kent0505 <78997827+kent0505@users.noreply.github.com>
2021-05-01 15:17:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let switchMode = document.getElementById('switchMode');
+
+switchMode.onclick = function () {
+    let theme = document.getElementById('theme');
+
+    if (theme.getAttribute('href') == 'light.css') {
+        theme.href = 'dark.css';
+    } else {
+        theme.href = 'light.css';
+    }
+}
